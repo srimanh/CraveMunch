45f473e71f2f42cdbb9e2b32f07a27d4a7085701
srimanh <srimandgl2004@gmail.com>
2023-12-20 17:34:17 +0530

    done changes

---
+++ anotherpage.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const leftContainer = document.getElementById('leftContainer');
+    const rightContainer = document.getElementById('rightContainer');
+
+    // Function to get the meal details by ID
+    async function getMealDetailsById(mealId) {
+        const apiUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            if (data.meals && data.meals.length > 0) {
+                const meal = data.meals[0];
+                displayMealDetails(meal);
+            } else {
+                leftContainer.innerHTML = '<p>No details found for this meal.</p>';
+                rightContainer.innerHTML = '';
+            }
+        } catch (error) {
+            console.error('Error fetching meal details:', error);
+        }
+    }
+
+    // Function to display meal details on the page
+    function displayMealDetails(meal) {
+        // Display the clicked image on the left side
+        leftContainer.innerHTML = `<img src="${meal.strMealThumb}" alt="${meal.strMeal}">`;
+
+        // Display ingredients on the right side
+        const ingredients = getIngredientsArray(meal);
+        const html = `
+            <h2>${meal.strMeal}</h2>
+            <h3>Ingredients:</h3>
+            <ol>${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}</ol>
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        `;
+
+        rightContainer.innerHTML = html;
+    }
+
+    // Function to get an array of ingredients from the meal object
+    function getIngredientsArray(meal) {
+        const ingredients = [];
+
+        // Loop through ingredients (up to 20, assuming maximum number of ingredients)
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = meal[`strIngredient${i}`];
+            const measure = meal[`strMeasure${i}`];
+
+            // Break the loop if no more ingredients
+            if (!ingredient) break;
+
+            ingredients.push(`${measure} ${ingredient}`);
+        }
+
+        return ingredients;
+    }
+
+    // Get the mealId from the URL query parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const mealId = urlParams.get('mealId');
+
+    // Fetch and display meal details
+    if (mealId) {
+        getMealDetailsById(mealId);
+    } else {
+        leftContainer.innerHTML = '<p>No meal ID provided.</p>';
+        rightContainer.innerHTML = '';
+    }
+});
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// random meal
 function fetchAndDisplayRandomMeal() {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
         .then(response => response.json())
@@ -22,3 +23,80 @@ function updateMealImage(imageUrl, mealName) {
 
 // Fetch and display a random meal when the page loads
 fetchAndDisplayRandomMeal();
+// link the api for search
+document.addEventListener('DOMContentLoaded', function () {
+    const foodGrid = document.getElementById('foodGrid');
+    const searchInput = document.getElementById('searchInput');
+
+    async function searchMeals() {
+        const searchTerm = searchInput.value.trim();
+
+        if (searchTerm !== '') {
+            const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+
+            try {
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+
+                // Clear existing content in foodGrid
+                foodGrid.innerHTML = '';
+
+                if (data.meals) {
+                    data.meals.forEach(meal => {
+                        // Create a container for each meal
+                        const container = document.createElement('div');
+                        container.classList.add('container1'); // Add the 'container' class
+
+                        // Create an image element with the 'container' class
+                        const image = document.createElement('img');
+                        image.src = meal.strMealThumb;
+                        image.alt = meal.strMeal;
+                        image.classList.add('container1'); // Add the 'container' class to the image
+
+                        // Create a paragraph for the name
+                        const nameParagraph = document.createElement('p');
+                        nameParagraph.textContent = meal.strMeal;
+
+                        // Add a click event listener to navigate to the instruction page
+                        container.addEventListener('click', function () {
+                            navigateToAnotherPage(meal.idMeal);
+                        });
+
+                        // Append the image and name to the container
+                        container.appendChild(image);
+                        container.appendChild(nameParagraph);
+
+                        // Append the container to the foodGrid
+                        foodGrid.appendChild(container);
+                    });
+                } else {
+                    const noResultsMessage = document.createElement('p');
+                    noResultsMessage.textContent = 'No results found.';
+                    foodGrid.appendChild(noResultsMessage);
+                }
+            } catch (error) {
+                console.error('Error fetching meals:', error);
+            }
+        }
+    }
+
+    function navigateToAnotherPage(mealId) {
+        // Construct the URL for the instruction page using the mealId
+        const anotherPageUrl = `anotherpage.html?mealId=${mealId}`;
+
+        // Navigate to the instruction page
+        window.location.href = anotherPageUrl;
+    }
+    
+    searchButton.addEventListener('click', function () {
+        searchMeals();
+    });
+
+
+    // Add an event listener for the Enter key in the search input
+    searchInput.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            searchMeals();
+        }
+    });
+});
